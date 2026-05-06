ac6ada897d16035f1d399d507f642ead170578f6
DESKTOP-P1PQC3Q\EngNoha <nohah938@gmail.com>
2026-05-06 22:29:01 +0300

    create Register cubit

--- lib/features/Auth/Register/manger/register_cubit.dart
+++ lib/features/Auth/Register/manger/register_cubit.dart
@@ -11,27 +11,41 @@ part 'register_state.dart';
 
 class RegisterCubit extends Cubit<RegisterState> {
   RegisterCubit() : super(RegisterInitial());
+
   ApiService apiService = ApiService(Dio());
+
   TextEditingController email = TextEditingController();
   TextEditingController pass = TextEditingController();
   TextEditingController name = TextEditingController();
   TextEditingController phone = TextEditingController();
 
-  void register  ({required email , required pass, required name ,
-    required phone }) async {
+  void register({
+    required String email,
+    required String pass,
+    required String name,
+    required String phone,
+  }) async {
     emit(LoadingRegister());
-    apiService.postData(
-      url: "api/auth/register/",
-        data: {"email": email, "password": pass,
-        "phone":phone , "name":name
-        }).then((res) async {
+
+    apiService
+        .postData(
+      url: "api/auth/register",
+      data: {
+        "email": email,
+        "password": pass,
+        "phone": phone,
+        "name": name,
+      },
+    )
+        .then((res) {
       final model = RegisterModel.fromJson(res.data);
+
       CacheHelper.saveToken(model.data?.token ?? '');
+
       emit(SuccessRegister());
     }).catchError((e) {
       emit(FailureRegister(e.toString()));
     });
   }
-
-
+}
 
